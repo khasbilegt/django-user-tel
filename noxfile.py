030dc59ba6ddcceae4c3fdb86eb9c7de78e0f365
khasbilegt <khasbilegt.ts@gmail.com>
2022-10-20 12:48:03 +0800

    run nox with no env

--- noxfile.py
+++ noxfile.py
@@ -5,9 +5,9 @@ import nox
 @nox.parametrize("django", ["3.2", "4.0", "main"])
 def test(session, django):
     if django == "main":
-        session.install("https://github.com/django/django/archive/main.tar.gz")
+        session.run("pip", "install", "https://github.com/django/django/archive/main.tar.gz")
     else:
-        session.install(f"django=={django}")
+        session.run("pip", "install", f"django=={django}")
     session.run("python", "makemigrations.py")
     session.run("coverage", "run", "runtests.py", external=True)
     session.run("coverage", "report", external=True)
